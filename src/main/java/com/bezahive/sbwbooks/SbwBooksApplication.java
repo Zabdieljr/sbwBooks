a926f91191afc43482aab118f12940e6ffef5a82
Zabdiel Elias <zabdieljr2@gmail.com>
2023-11-01 10:23:37 -0500

    project init

---
+++ src/main/java/com/bezahive/sbwbooks/SbwBooksApplication.java
@@ -0,0 +1,13 @@
+package com.bezahive.sbwbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbwBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bezahive/sbwbooks/SbwBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezahive.sbwbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
